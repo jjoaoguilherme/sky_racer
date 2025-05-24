cc90b36157b30e4ad6228f078e1b61efc8bc209b
LucazinnDEV <141593793+LucazinnDEV@users.noreply.github.com>
2025-05-24 19:37:44 -0300

    Update nave.c finalização da nave

--- src/nave.c
+++ src/nave.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <ncurses.h>
 
 #include "screen.h"
 #include "keyboard.h"
@@ -9,13 +10,23 @@
 Nave* criar_nave() {
     Nave *n = malloc(sizeof(Nave));
     n->x = screenGetWidth() / 2;
-    n->y = screenGetHeight() - 2;
+    n->y = screenGetHeight() - 4; 
     return n;
 }
 
+
 void mover_nave(Nave *n, int comando) {
-    if (comando == KEY_LEFT && n->x > 1) n->x--;
-    if (comando == KEY_RIGHT && n->x < screenGetWidth() - 2) n->x++;
+    if (comando == KEY_LEFT  && n->x > 2)                  n->x--;
+    if (comando == KEY_RIGHT && n->x < screenGetWidth() - 3) n->x++;
+    if (comando == KEY_UP    && n->y > 3)                  n->y--;
+    if (comando == KEY_DOWN  && n->y < screenGetHeight() - 1) n->y++;
+}
+
+void desenhar_nave(Nave *n) {
+    screenSetCharXY(n->x,     n->y - 3, '^');
+    screenSetStringXY(n->x - 1, n->y - 2, "/ \\");
+    screenSetStringXY(n->x - 1, n->y - 1, "|O|");
+    screenSetStringXY(n->x - 2, n->y,     "/| |\\");
 }
 
 void destruir_nave(Nave *n) {
