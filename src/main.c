1c2f8e7f1484f30058214481e21fbf7f37fb2699
Rafaellc-DEV <rafaelcosta.2600@gmail.com>
2025-05-24 00:44:42 -0300

    Implementando menu + solicitar nome do jogador

--- src/main.c
+++ src/main.c
@@ -1,6 +1,35 @@
+#include <stdio.h>
+#include <string.h>
 #include "jogo.h"
 
 int main() {
-    iniciar_jogo();
+    int opcao;
+    char nome[50] = "";
+
+    while (1) {
+        printf("\n=== SKY RACER ===\n");
+        printf("1. Jogar\n");
+        printf("2. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+        getchar(); 
+
+        if (opcao == 1) {
+            if (strlen(nome) == 0) {
+                printf("Digite seu nome: ");
+                fgets(nome, 50, stdin);
+                nome[strcspn(nome, "\n")] = 0; 
+            } else {
+                printf("Nome já registrado: %s\n", nome);
+            }
+            iniciar_jogo(nome);
+        } else if (opcao == 2) {
+            printf("Saindo...\n");
+            break;
+        } else {
+            printf("Opção inválida.\n");
+        }
+    }
+
     return 0;
 }
