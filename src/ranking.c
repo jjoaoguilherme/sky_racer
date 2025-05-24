d9692b749504fdee96f5d8933e97a27e88f0fc22
Rafaellc-DEV <rafaelcosta.2600@gmail.com>
2025-05-24 00:54:55 -0300

    Implementando a adicionar os nomes e os pontos no arquivo .txt para salvar a pontuação

--- src/ranking.c
+++ src/ranking.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "ranking.h"
 
-void salvar_pontuacao(int pontos) {
+void salvar_pontuacao(const char *nome, int pontos) {
     FILE *f = fopen("assets/ranking.txt", "a");
     if (f) {
-        fprintf(f, "%d\n", pontos);
+        fprintf(f, "%s - %d pontos\n", nome, pontos);
         fclose(f);
     }
 }
--- src/ranking.h
+++ src/ranking.h
@@ -1,6 +1,6 @@
 #ifndef RANKING_H
 #define RANKING_H
 
-void salvar_pontuacao(int pontos);
+void salvar_pontuacao(const char *nome, int pontos);
 
 #endif
